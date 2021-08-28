db08f35fd645cae52d44b9696aedc905067bde13
Junyao Shi <junyao.shi@columbia.edu>
2021-08-27 17:47:55 -0700

    revert bad docker run file changes

--- docker/base/run.sh
+++ docker/base/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --gpus all -ti --rm igibson/igibson:latest -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix
+docker run --gpus all -ti --rm igibson/igibson:latest
